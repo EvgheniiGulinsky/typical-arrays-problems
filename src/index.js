3251e8b724154d4bf1e1d5d390298aee9add38b8
Yauheni Hulinski <riggedman22@gmail.com>
2020-03-18 21:36:27 +0300

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-
 exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+    if (!array || array=="") return 0
+    return array.sort(function(a, b){return a - b})[0]
+    }
+    
+    exports.max = function max (array) {
+    if (!array || array=="") return 0
+    return array.sort(function(a, b){return a - b})[array.length-1]
+    }
+    
+    exports.avg = function avg (array) {
+    if (!array || array=="")
+    return 0
+    let sum=0
+    for (let i = 0; i < array.length; i++) {
+    sum = sum + array[i]
+    }
+    return sum/array.length
+    }
+    
